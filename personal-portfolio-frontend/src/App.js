8d6aec258b916a61bf99b4b9388b15ec20628e88
Nick <nfelix1997@gmail.com>
2021-09-30 17:51:19 -0400

    React Routing

--- personal-portfolio-frontend/src/App.js
+++ personal-portfolio-frontend/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
+import {BrowserRouter as Router,Route, Switch}  from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
 import ListProvidedServicesComponent from './components/ListProvidedServicesComponent';
 
 function App() {
+
+
+
   return (
     <div>
-
-      <HeaderComponent />
-      <div className="container">
-        <ListProvidedServicesComponent />
-      </div>
-      <FooterComponent />
+      <Router>
+          <HeaderComponent />
+            <div className="container">
+              <Switch>
+                <Route path = "/" component = {ListProvidedServicesComponent}></Route>
+                <Route path = "/services" component = {ListProvidedServicesComponent}></Route>
+                <ListProvidedServicesComponent />
+              </Switch>
+            </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
