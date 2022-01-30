9e8ffcefbec547847095ff6236776441b84ff705
Nick <nfelix1997@gmail.com>
2022-01-29 22:28:42 -0500

    Project file cleanup

--- personal-portfolio-frontend/src/App.js
+++ personal-portfolio-frontend/src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch}  from 'react-router-dom';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
-import ListProvidedServicesComponent from './components/ListProvidedServicesComponent';
-import CreateProvidesServicesComponent from './components/CreateProvidesServicesComponent';
 import ListProductComponent from './components/ListProductComponent';
 
 function App() {
@@ -15,8 +13,7 @@ function App() {
             <div className="container">
               <Switch>
                 <Route path = "/" exact component = {ListProductComponent}></Route>
-                <Route path = "/services" component = {ListProvidedServicesComponent}></Route>
-                <Route path = "/add-service" component = {CreateProvidesServicesComponent}></Route>
+                
               </Switch>
             </div>
           <FooterComponent />
