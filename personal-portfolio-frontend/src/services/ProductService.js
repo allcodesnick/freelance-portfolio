f70fde4880702e2d767e93f29fb4994d93afdfec
Nick <nfelix1997@gmail.com>
2022-01-29 22:17:49 -0500

    Product Service Url change

--- personal-portfolio-frontend/src/services/ProductService.js
+++ personal-portfolio-frontend/src/services/ProductService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const PROVIDED_SERVICES_API_BASE_URL = "http://localhost:8080/api/v1/products";
+const PROVIDED_SERVICES_API_BASE_URL = "http://localhost:8080/api/v1/";
 
 class ProductService{
 
     getProducts(){
-        return axios.get(PROVIDED_SERVICES_API_BASE_URL);
+        return axios.get(PROVIDED_SERVICES_API_BASE_URL  + '/products');
     }
 }
 
