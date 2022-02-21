bfed564cfbf1a172b616d9a0935346d55fd2991e
Nick <nfelix1997@gmail.com>
2022-02-21 12:30:02 -0500

    Product Service update

--- personal-portfolio-frontend/src/services/ProductService.js
+++ personal-portfolio-frontend/src/services/ProductService.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 
-const PROVIDED_SERVICES_API_BASE_URL = "http://localhost:8080/api/v1/";
+const PROVIDED_SERVICES_API_BASE_URL = "http://localhost:8080/api/v1/products";
 
 class ProductService{
 
     getProducts(){
-        return axios.get(PROVIDED_SERVICES_API_BASE_URL  + 'products');
+        return axios.get(PROVIDED_SERVICES_API_BASE_URL);
     }
 
     getProductsById(productId){
-        return axios.get(PROVIDED_SERVICES_API_BASE_URL + 'products/' + productId);
+        return axios.get(PROVIDED_SERVICES_API_BASE_URL + '/' + productId);
     }
 
     createProduct(product){
-        return axios.post(PROVIDED_SERVICES_API_BASE_URL + 'products', product);
+        return axios.post(PROVIDED_SERVICES_API_BASE_URL, product);
     }
 
     updateProduct(productId, product){
-        return axios.put(PROVIDED_SERVICES_API_BASE_URL + 'products/' + productId, product);
+        return axios.put(PROVIDED_SERVICES_API_BASE_URL + '/' + productId, product);
     }
 
     deleteProduct(productId){
-        return axios.delete(PROVIDED_SERVICES_API_BASE_URL + 'products/' + productId);
+        return axios.delete(PROVIDED_SERVICES_API_BASE_URL + '/' + productId);
     }
 }
 
