f91e2f5ba2a691e95085303afbfaaa64362deab6
Nick <nfelix1997@gmail.com>
2022-02-24 20:12:58 -0500

    Update

--- personal-portfolio-frontend/src/components/FooterComponent.jsx
+++ personal-portfolio-frontend/src/components/FooterComponent.jsx
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
 
-class FooterComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-    
-
-    render() {
-        return (
-            <div>
-                <footer className="text-muted">
-                    All rights Reserved @allcodesnick
-                </footer>
-                
-            </div>
-        );
-    }
+function FooterComponent () {
+    return (
+        <div className="fixed-bottom">                    
+            <footer>
+                All rights Reserved @allcodesnick
+            </footer>
+        </div>
+    );
+        
 }
 
 export default FooterComponent;
--- personal-portfolio-frontend/src/components/HeaderComponent.jsx
+++ personal-portfolio-frontend/src/components/HeaderComponent.jsx
@@ -1,38 +1,25 @@
-import React, { Component } from 'react';
-
-class HeaderComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-    
-
-    render() {
-        return (
-           <div>
-               <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div>
-                        <a href="https://github.com/allcodesnick" className="navbar-brand" target="_blank"rel="noreferrer" >Github Link</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a className="nav-item nav-link" href="/">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a className="nav-item nav-link" href="/services/view-all">Services</a>
-                            </li>
-                            <li class="nav-item">
-                                <a className="nav-item nav-link disabled" href="/#">Projects</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-           </div>
-        );
-    }
+function HeaderComponent() {
+    /* div className="collapse navbar-collapse" id="navbarNav" */
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                 <a href="https://github.com/allcodesnick" className="navbar-brand" target="_blank"rel="noreferrer" >My Github</a>
+                 <div> 
+                     <ul className="navbar-nav">
+                         <li className="nav-item active">
+                             <a className="nav-item nav-link" href="/">Home</a>
+                         </li>
+                         <li className="nav-item">
+                             <a className="nav-item nav-link" href="/services/view-all">Services</a>
+                         </li>
+                         <li className="nav-item">
+                             <a className="nav-item nav-link disabled" href="/#">Projects</a>
+                         </li>
+                     </ul>
+                 </div>
+             </nav>
+        </div>
+     );
 }
 
 export default HeaderComponent;
