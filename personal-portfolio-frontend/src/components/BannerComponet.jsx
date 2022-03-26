17f5f818240edfb59b315cde327685bcfc1f0a23
Nick <nfelix1997@gmail.com>
2022-03-26 11:27:34 -0400

    outer banner

--- personal-portfolio-frontend/src/components/BannerComponet.jsx
+++ personal-portfolio-frontend/src/components/BannerComponet.jsx
@@ -5,8 +5,11 @@ import '../Banner.css'
 class BannerComponet extends Component {
     render() {
         return (
-            <div className='header'>
-                <h1>This is a Banner</h1>
+            <div className='outer-banner'>
+                <div>
+                    <h1>This is a Banner</h1>
+                    <p>About me: </p>
+                </div>
             </div>
         );
     }
