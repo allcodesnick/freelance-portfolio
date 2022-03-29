9a181558e0ae8d44ea34754383d7f6fe22e30597
Nick <nfelix1997@gmail.com>
2022-03-28 22:45:06 -0400

    portfolio image

--- personal-portfolio-frontend/src/components/BannerComponet.jsx
+++ personal-portfolio-frontend/src/components/BannerComponet.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import '../Banner.css';
-import portfolioImage from '../images/portfolio-image.png';
+import portfolioImage from '../images/portfolio-image.jpg';
 
 
 class BannerComponet extends Component {
     render() {
         return (
-            <div className='outer-banner'>
-                <div>
-                    <img src={portfolioImage} alt="" id='banner-portfolio-image' />
-
-                </div>
+            <div id='outer-banner'>
                 
-                <div className='banner-card'>
+                
+                <div id='banner-card'>
                     <h1>Hello World!</h1>
                     <p>About me: </p>
                 </div>
+                <div>
+                        <img src={portfolioImage} alt="" id='banner-portfolio-image' width="250" height="400"/>
+                    </div>
             </div>
             
         );
