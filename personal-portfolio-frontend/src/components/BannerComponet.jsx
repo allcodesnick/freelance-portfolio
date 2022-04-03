62427e5efc68856c17e6284aa84aaf22d76b9333
Nick <nfelix1997@gmail.com>
2022-04-03 14:41:47 -0400

    banner ui

--- personal-portfolio-frontend/src/components/BannerComponet.jsx
+++ personal-portfolio-frontend/src/components/BannerComponet.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import '../Banner.css';
-// import portfolioImage from '../images/portfolio-image.jpg';
+import portfolioImage from '../images/my_image.jpeg';
 
 
 class BannerComponet extends Component {
     render() {
-        /** <img src={portfolioImage} alt="profileImage" id='banner-portfolio-image' /> */
         return (
             <div id='outer-banner'>
                 <div id='banner-card'>
-                    <h1>Hello World!</h1>
-                    <p>About me: Contrary to popular belief, Lorem Ipsum is not simply random text.
-                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the</p>
+                    <div class="row g-0">
+                        <div class="col-sm-6 col-md-8">
+                            <h1>Hello World!</h1>
+                            <p>About me: Contrary to popular belief, Lorem Ipsum is not simply random text.
+                                It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                                Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the</p>
+                        </div>
+                        <br />
+                        <div class="col-6 col-md-4">
+                            <img src={portfolioImage} alt="profileImage" height="300px"/>
+                        </div>
+                    </div>
                 </div>
             </div>
             
