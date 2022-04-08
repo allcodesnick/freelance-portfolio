288c9480010158f1db2767cd99a2b0d0a34081e2
Nick <nfelix1997@gmail.com>
2022-04-08 01:03:20 -0400

    banner ui

--- personal-portfolio-frontend/src/components/BannerComponet.jsx
+++ personal-portfolio-frontend/src/components/BannerComponet.jsx
@@ -4,12 +4,7 @@ import portfolioImage from '../images/my_image.jpeg';
 
 
 class BannerComponet extends Component {
-    /** i might hae to change "col-6 col-md-4" for img and create a div to fill the image and resize automatically
-     * <p>About me: Contrary to popular belief, Lorem Ipsum is not simply random text.
-                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the
-                        <img src={portfolioImage} alt="profileImage" id='profile-image' className='img'/></p>
-     */
+
     render() {
         return (
             <div id='outer-banner'>
@@ -18,7 +13,7 @@ class BannerComponet extends Component {
                     <img src={portfolioImage} alt="profileImage"/>
                     <br />
                     <br />
-                    <p>About me:<br /> Contrary to popular belief, Lorem Ipsum is not simply random text.
+                    <p>About me:<br /> &emsp; Contrary to popular belief, Lorem Ipsum is not simply random text.
                         It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
                         Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia.About me: Contrary to popular belief, Lorem Ipsum is not simply random text.
                         It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
